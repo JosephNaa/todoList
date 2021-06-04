9eb80f5990631c8d1e95f84e3661523991a2065c
JosephNaa <js.pekah@gmail.com>
2021-06-04 16:38:02 +0900

    design ui

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world!</Text>
-      <StatusBar style="auto" />
+
+      {/* Today's Tasks */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+      </View>
+
+      <View style={styles.items}>
+        {/* This is where thetasks will go! */}
+        <Task text={'Task 1'}/>
+        <Task text={'Task 2'}/>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +24,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30
   },
 });
